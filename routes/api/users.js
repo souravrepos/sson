5e848bfe941c52a6a038d13a8a409e62727be70b
SKT <souravrepos@gmail.com>
2020-04-18 08:46:10 +0530

    Initial Commit for SSON project

---
+++ routes/api/users.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+// route : api/users
+router.post(
+  "/",
+  [
+    check("name", "Please enter a name.").not().notEmpty(),
+    check("email", "Please enter a valid email.").isEmail(),
+    check(
+      "password",
+      "Please enter a password with minimum 8 characters"
+    ).isLength({ min: 8 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      // Check if user already exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      // creating an intance of a user
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      // encrypting the password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // saving user data in DB
+      await user.save();
+
+      // returning json web token
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+module.exports = router;
